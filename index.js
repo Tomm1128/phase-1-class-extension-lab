2839bed986da15152a36b8574e532d42e79de228
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-04 10:03:04 -0400

    Create Triangle class

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ class Polygon {
     return this.sides.reduce((acc, side) => acc + side, 0)
   }
 }
+
+class Triangle extends Polygon {
+  get isValid(){
+    const [side1, side2, side3] = this.sides
+
+    const pair1 = side1 + side2 > side3
+    const pair2 = side2 + side3 > side1
+    const pair3 = side3 + side1 > side2
+
+    if (pair1 && pair2 && pair3){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+}
